b6b4e484a73d64edd0cb85dd1851ccbde4891ccb
KennethN <kennethnathanael31@gmail.com>
2026-05-26 21:54:21 +0700

    penyesuaian2

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -3,21 +3,23 @@ export default function Pagination() {
     return (
         <div className="pagination">
             <div class="mt-6 flex items-center justify-end gap-2">
+                {/* Sementara pake activeClass="" biar bisa di warnanya itu g detect .active di css soalny blm ad perbedaan alamat */}
+                {/* Todo : Hapus activeclass nanti */}
                 {/* Prev */}
-                <A class="pagination-btn">
+                <A href="#" activeClass="" class="pagination-btn">
                     {"<-"} Prev
                 </A>
 
                 {/* Active Page */}
-                <A class="pagination-btn active">1</A>
+                <A href="#" activeClass="" class="pagination-btn active">1</A>
 
                 {/* Normal Page */}
-                <A class="pagination-btn">2</A>
+                <A href="#" activeClass="" class="pagination-btn">2</A>
 
-                <A class="pagination-btn">3</A>
+                <A href="#" activeClass="" class="pagination-btn">3</A>
 
                 {/* Next */}
-                <A class="pagination-btn">Next {"->"}</A>
+                <A href="#" activeClass="" class="pagination-btn">Next {"->"}</A>
             </div>
         </div>
     );
