391bfa1ce9048fd9c69297153556c94c63c8c1cc
KennethN <kennethnathanael31@gmail.com>
2026-05-26 21:21:01 +0700

    penyesuaian tailwind

--- src/Back.jsx
+++ src/Back.jsx
@@ -3,8 +3,8 @@ import { A } from "@solidjs/router";
 
 export default function BackButton() {
   return (
-    <A href="/myevent" class="inline-block px-4 py-2 border rounded border-black-400 text-black-700">
-        Kembali ke Event
+    <A href="/myevent" class="inline-block px-4 py-2 border border-black-300 text-black-700 font-semibold rounded hover:border-indigo-500 hover:text-indigo-500 transition-colors">
+      {"< "}Kembali ke Event
     </A>
   );
 }
--- src/EventHeader.jsx
+++ src/EventHeader.jsx
@@ -2,10 +2,10 @@ import { BatalPendaftaran } from "./js/BatalPendaftaran";
 
 export default function EventHeader() {
   return (
-    <div class="flex justify-between items-center mb-6 border-b pb-4 mt-4">
-        <h1 class="font-bold text-black text-3xl">Parcestra</h1>
-        <button class="px-4 py-2 border border-whitw-500 text-black-500 font-semibold rounded" 
-        type="button" 
+    <div className="flex justify-between items-center mb-6 border-b pb-4 mt-4">
+      <h1 className="font-bold text-black text-3xl">Parcestra</h1>
+      <button className="px-4 py-2 border border-black-300 text-black-700 font-semibold rounded hover:border-indigo-500 hover:text-indigo-500 transition-colors"
+        type="button"
         onClick={() => BatalPendaftaran(props.eventId)}> Batal Pendaftaran </button>
     </div>
   );
